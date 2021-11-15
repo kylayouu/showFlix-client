5245e6f07dd7427b15820352357bf3d6e0358e6a
Kyla Quilos <quilos.kyla@gmail.com>
2021-11-15 15:33:08 -0800

    updated movie card component

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
@@ -7,4 +8,13 @@ class MovieCard extends React.Component {
   }
 }
 
-export default MovieCard;
+MovieCard.propTypes = {
+	movie: PropTypes.shape({
+		Title: PropTypes.string.isRequired,
+		Description: PropTypes.string.isRequired,
+    ImagePath: PropTypes.string.isRequired
+	}).isRequired,
+	onMovieClick: PropTypes.func.isRequired
+};
+
+export default MovieCard
