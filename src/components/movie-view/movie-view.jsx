374f3df572e198d46b4e47e1268751dfa585b3a5
Kyla Quilos <quilos.kyla@gmail.com>
2021-11-15 15:33:25 -0800

    updated movie view component

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -2,11 +2,19 @@ import React from "react";
 
 class MovieView extends React.Component {
 
+	keypressCallback(event) {
+    console.log(event.key);
+  }
+
 	componentDidMount() {
 		document.addEventListener('keypress', event => {
 			console.log(event.key);
 		});
 	}
+
+	componentWillUnmount() {
+    document.removeEventListener('keypress', this.keypressCallback);
+  }
 	
 	render() {
 		const { movie, onBackClick } = this.props;
@@ -23,14 +31,6 @@ class MovieView extends React.Component {
 				<div className='movie-description'>
 					<span className='label'>Description: </span>
 					<span className='value'>{movie.Description}</span>
-				</div>
-				<div className='movie-genre'>
-					<span className='label'>Genre: </span>
-					<span className='value'>{movie.Genre}</span>
-				</div>
-				<div className='movie-directors'>
-					<span className='label'>Director: </span>
-					<span className='value'>{movie.Director}</span>
 				</div>
 				<button onClick={() => { onBackClick(null); }} >Back</button>
 
